d1593b7ddb72438d6ac423418184f90557eb7d39
brook <brookrodia@gmail.com>
2023-12-11 01:06:33 -0500

    form builder

---
+++ payload/src/formBuilder.config.ts
@@ -0,0 +1,44 @@
+// formBuilderConfig.js
+
+const formBuilderConfig = {
+    fields: {
+        text: true,
+        textarea: true,
+        select: true,
+        email: true,
+        state: true,
+        country: true,
+        checkbox: true,
+        number: true,
+        message: true,
+        payment: true
+    },
+    handlePayment: async ({ form, submissionData }) => {
+        // Payment handling logic here
+        // Example: calculate the total cost, integrate third-party payment processing API
+    },
+    beforeEmail: (emailsToSend) => {
+        // Email customization logic here
+        // Example: modify the emails in any way before they are sent
+    },
+    formOverrides: {
+        slug: "customForms", // Custom slug for forms
+        access: {
+            read: () => true,
+            update: () => true,
+        },
+        fields: [
+            {
+                name: "customField",
+                type: "text"
+            }
+            // ... other custom fields if necessary
+        ]
+    },
+    formSubmissionOverrides: {
+        slug: "custom-submissions", // Custom slug for form submissions
+        // Other custom settings for form submissions
+    }
+};
+
+export default formBuilderConfig;
