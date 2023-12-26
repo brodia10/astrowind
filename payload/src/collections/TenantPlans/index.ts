17231ccc695a5686f52ba8b478b0f061abaf0bfd
brook <brookrodia@gmail.com>
2023-12-26 16:18:21 -0500

    Tenant Plans model

---
+++ payload/src/collections/TenantPlans/index.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from 'payload/types';
+import { tenants } from '../Pages/access/tenants';
+import { tenantAdmins } from '../Tenants/access/tenantAdmins';
+
+const TenantPlans: CollectionConfig = {
+    slug: 'tenant-plans',
+    admin: {
+        useAsTitle: 'title',
+    },
+    access: {
+        create: tenantAdmins,
+        read: tenants,
+        update: tenantAdmins,
+        delete: tenantAdmins,
+    },
+    fields: [
+        {
+            name: 'tenant',
+            type: 'relationship',
+            relationTo: 'tenants',
+        },
+        // ... define fields like title, price, features, isActive, etc.
+        // ... similar to global plans but specific to each tenant's offerings
+    ],
+};
+
+export default TenantPlans;
