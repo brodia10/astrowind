25deb19c24537474b0175858e3f76e46077004d0
brook <brookrodia@gmail.com>
2024-05-05 12:16:11 -0400

    Create site button

--- payload/src/components/CreateSiteButton.tsx
+++ payload/src/components/CreateSiteButton.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 
-const CreateSiteButton = () => {
-    const buttonContainerStyle = {
-        position: 'absolute', // Positioning relative to the nearest positioned ancestor
-        top: '15%', // Adjust top as necessary, assuming the parent has a set height
-        right: '5%', // Align to the right
-        left: '10',
-        transform: 'translateY(-50%)' // This will vertically center it
-    };
+// Define the styles using TypeScript's type annotations
+interface StyleProps {
+    buttonContainerStyle: React.CSSProperties;
+    buttonStyle: React.CSSProperties;
+}
 
-    const buttonStyle = {
-        background: 'white',
-        border: '1px solid #FF5722',
-        borderRadius: '50%',
-        cursor: 'pointer',
-        width: '50px',
-        height: '50px',
-        boxShadow: '0px 5px 8px #00000029',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '24px',
-        color: '#FF5722',
-        // Ensure the button doesn't shrink or grow in a flex container
-        flexShrink: 0
+const CreateSiteButton: React.FC = () => {
+    const styles: StyleProps = {
+        buttonContainerStyle: {
+            position: 'absolute',
+            top: '16%',
+            right: '5%',
+            left: '10',
+            transform: 'translateY(-50%)',
+        },
+        buttonStyle: {
+            background: 'white',
+            border: '1px solid #FF5722',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            width: '50px',
+            height: '50px',
+            boxShadow: '0px 5px 8px #00000029',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '24px',
+            color: '#FF5722',
+            flexShrink: 0,
+        }
     };
 
     const onClick = () => {
-        window.location.href = 'http://localhost:3000/admin/collections/tenants/create'
+        window.location.href = `/admin/collections/tenants/create`;
     }
 
     return (
-        <div style={buttonContainerStyle}>
-            <button style={buttonStyle} onClick={onClick}>
+        <div style={styles.buttonContainerStyle}>
+            <button style={styles.buttonStyle} onClick={onClick}>
                 +
             </button>
         </div>
